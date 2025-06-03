fba27fc56a02a0568e603dfc27a4058449ea0f32
TEST <test@test.com>
2025-06-03 20:55:15 +0300

    Задание

--- src/main.py
+++ src/main.py
@@ -8,6 +8,10 @@ def main():
     names = read_names(config.READ_PATH)
     formated_names = format_names(names)
     write_names(formated_names, config.WRITE_PATH)
+    # Задание: писать инициалы отсортированными по алфавиту (по фамилии)
+    # Дописать README.md
+    # Задание*: писать порядковый номер и в формате csv
+    # `*example.csv`
 
 
 main()
