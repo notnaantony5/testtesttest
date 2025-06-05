0e7a4119b53399598f2ec33d873836ad10e28695
TEST <test@test.com>
2025-06-05 20:45:23 +0300

    Пакеты

--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
-from field_utils import generate_field
+from field_utils import generate_field, write_cell, tranform_coord_to_index
 from printing import print_field, print_hello
+from user_input import get_user_coordinates_input
 
 
 def main():
     print_hello()
     field = generate_field()
+    print_field(field)
+    while True:
+        coord = get_user_coordinates_input()
+        idx = tranform_coord_to_index(coord)
+        if write_cell(field, idx):
+            break
+        print("Ячейка занята")
     print_field(field)
 
 
