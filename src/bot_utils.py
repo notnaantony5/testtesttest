27d35182a922cbe6ad42b5d176d9041565353703
TEST <test@test.com>
2025-06-05 20:53:12 +0300

    Пакеты

---
+++ src/bot_utils.py
@@ -0,0 +1,2 @@
+def get_turn(field: list[list[str]]) -> None:
+    pass
--- src/field_utils.py
+++ src/field_utils.py
@@ -1,3 +1,18 @@
+COMBINATIONS = [
+    ((0, 0), (0, 1), (0, 2)),
+    ((1, 0), (1, 1), (1, 2)),
+    ((2, 0), (2, 1), (2, 2)),
+    ((0, 0), (1, 0), (2, 0)),
+    ((0, 1), (1, 1), (2, 1)),
+    ((0, 2), (1, 2), (2, 2)),
+    ((0, 0), (1, 1), (2, 2)),
+    ((0, 2), (1, 1), (2, 0)),
+]
+
+LETTER_MAP = {"A": 0, "B": 1, "C": 2}
+NUMBER_MAP = {"1": 0, "2": 1, "3": 2}
+
+
 def generate_field():
     field = [
         [" ", " ", " "],
@@ -5,10 +20,6 @@ def generate_field():
         [" ", " ", " "],
     ]
     return field
-
-
-LETTER_MAP = {"A": 0, "B": 1, "C": 2}
-NUMBER_MAP = {"1": 0, "2": 1, "3": 2}
 
 
 def tranform_coord_to_index(coords: tuple[str, str]) -> tuple[int, int]:
