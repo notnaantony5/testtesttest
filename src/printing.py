5627d9b577ebe40bec90551174c4645d689946eb
TEST <test@test.com>
2025-06-05 20:15:12 +0300

    Пакеты

--- src/printing.py
+++ src/printing.py
@@ -1,5 +1,7 @@
 HELLO_TEXT = "Привет!\nЭто крестики нолики!"
-SPLIT_LINE = "-------------\n"
+NUM_COORDS = "    1   2   3  \n"
+LETTER_COORDS = ("A", "B", "C")
+SPLIT_LINE = "  -------------\n"
 
 
 def print_hello() -> None:
@@ -7,9 +9,9 @@ def print_hello() -> None:
 
 
 def print_field(field: list[list[str]]) -> None:
-    field_str = SPLIT_LINE
-    for row in field:
-        field_str += "| "
+    field_str = NUM_COORDS + SPLIT_LINE
+    for row, letter in zip(field, LETTER_COORDS):
+        field_str += letter + " | "
         for item in row:
             field_str += item + " | "
         field_str += "\n" + SPLIT_LINE
