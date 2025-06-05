34750499e2d37e2185f19ec7ca0207c9451058d5
TEST <test@test.com>
2025-06-05 21:10:43 +0300

    Пакеты

--- src/bot_utils.py
+++ src/bot_utils.py
@@ -1,2 +1,38 @@
-def get_turn(field: list[list[str]]) -> None:
-    pass
+from field_utils import COMBINATIONS
+from random import choice
+
+
+def bot_turn(field: list[list[str]]) -> None:
+    groups: list[tuple[int, int, int, list[tuple[int, int]]]] = []
+    for comb in COMBINATIONS:
+        spaces = 0
+        x_count = 0
+        o_count = 0
+        blank_cells = []
+        for ridx, cidx in comb:
+            cell = field[ridx][cidx]
+            if cell == "X":
+                x_count += 1
+            elif cell == "O":
+                o_count += 1
+            else:
+                spaces += 1
+                blank_cells.append((ridx, cidx))
+        groups.append((spaces, x_count, o_count, blank_cells))
+    pri1 = []
+    pri2 = []
+    pri3 = []
+    for group in groups:
+        s, x, o, _ = group
+        if o == 2 and s == 1:
+            pri1.append(group)
+        elif x == 2 and s == 1:
+            pri2.append(group)
+        elif s > 0:
+            pri3.append(group)
+    for pri in [pri1, pri2, pri3]:
+        if pri:
+            _, _, _, cells = choice(pri)
+            ridx, cidx = choice(cells)
+            field[ridx][cidx] = "O"
+            return
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from bot_utils import bot_turn
 from field_utils import generate_field, write_cell, tranform_coord_to_index
 from printing import print_field, print_hello
 from user_input import get_user_coordinates_input
@@ -6,14 +7,15 @@ from user_input import get_user_coordinates_input
 def main():
     print_hello()
     field = generate_field()
-    print_field(field)
     while True:
-        coord = get_user_coordinates_input()
-        idx = tranform_coord_to_index(coord)
-        if write_cell(field, idx):
-            break
-        print("Ячейка занята")
-    print_field(field)
+        print_field(field)
+        while True:
+            coord = get_user_coordinates_input()
+            idx = tranform_coord_to_index(coord)
+            if write_cell(field, idx):
+                break
+            print("Ячейка занята")
+        bot_turn(field)
 
 
 if __name__ == "__main__":
