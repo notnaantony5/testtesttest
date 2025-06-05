84f7e5eee864218852ff03f368b6bcea41d52721
TEST <test@test.com>
2025-06-05 20:19:12 +0300

    Пакеты

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from field_utils import generate_field
-from printing import print_field
+from printing import print_field, print_hello
 
 
 def main():
+    print_hello()
     field = generate_field()
     print_field(field)
 
