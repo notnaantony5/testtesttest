ae45139e7f51f9b6f2b9010f635eca67f5e19f18
TEST <test@test.com>
2025-06-19 21:02:28 +0300

    ваыы

--- main4.py
+++ main4.py
@@ -7,6 +7,16 @@ def get_questions(path: str = "questions.json") -> list[dict[str, str]]:
         return json.load(f)
 
 
+def get_table(path: str = "results.json") -> list[dict[str, str | int]]:
+    with open(path, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+
+def write_table(table: list[dict[str, str | int]], path: str = "results.json") -> None:
+    with open(path, "w", encoding="utf-8") as f:
+        return json.dump(table, f, ensure_ascii=False, indent=4)
+
+
 def main():
     user_name = input("Ваше имя: ")
     start = datetime.now()
@@ -20,6 +30,9 @@ def main():
     end = datetime.now()
     time = int((end - start).total_seconds())
     print(f"Вы набрали {points} баллов за {time} с")
+    table = get_table()
+    table.append({"name": user_name, "points": points, "seconds": time})
+    write_table(table)
 
 
 if __name__ == "__main__":
