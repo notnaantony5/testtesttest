ef3fa8a4d27404f7f4c30fcb07ad0b1b1e89f7a7
TEST <test@test.com>
2025-06-24 20:53:25 +0300

    еуые

--- main2.py
+++ main2.py
@@ -1,3 +1,6 @@
+from pickletools import string1
+
+
 class User:
     def __init__(self, username: str):
         self.username = username
@@ -38,9 +41,33 @@ user1.write("********")
 admin1.mute(user1)
 user1.write("Я пошутил!")
 # class Animal:
+# class Cat(Animal):
 # class Wolf(Animal):
 # class Dog(Wolf):
+
 
 # Класс животное - имеет цвет
 # Класс волк имеет цвет, имеет возможность говорить say() -> "воет"
 # Класс собака имеет цвет, имеет возможность говорить say() -> "гав" и имеет имя (атрибут name)
+class Animal:
+    def __init__(self, color: str) -> None:
+        self.color = color
+
+
+class Cat(Animal):
+    def say(self):
+        print("Мяу")
+
+
+class Wolf(Animal):
+    def say(self):
+        print("воет")
+
+
+class Dog(Wolf):
+    def __init__(self, color: str, name: str) -> None:
+        super().__init__(color)
+        self.name = name
+
+    def say(self):
+        print("гав")
