7b12094dfdb9a6712b7cc91efe0242012b970e34
TEST <test@test.com>
2025-06-24 19:07:22 +0300

    init

--- main.py
+++ main.py
@@ -1,39 +1,58 @@
-NAMES = [
-    ("Ваня", 6, 20),
-    ("Петя", 7, 40),
-    ("Саша", 7, 50),
-]
-SPLIT_LINE = "-" * 46 + "\n"
+from datetime import datetime
 
 
-def ct(text: str | int, spaces: int, *, odd=True) -> str:
-    text = str(text)
-    if odd and len(text) % 2:
-        text += " "
-    return " " * spaces + text + " " * spaces
+class User:
+    def __init__(self, name: str):
+        self.name = name
 
 
-def format(names: list[tuple[str, int, int]]) -> str:
-    text = SPLIT_LINE
-    text += "|" + ct("Таблица лидеров", 14) + "|\n"
-    text += SPLIT_LINE
-    text += "|" + ct("Имя", 5)
-    text += "|" + ct("Очки", 5)
-    text += "|" + ct("Время, с", 3) + "|\n"
-    text += SPLIT_LINE
-    for name, points, time in NAMES:
-        text += "|" + ct(name, 5)
-        text += "|" + ct(points, 6)
-        text += "|" + ct(time, 6) + "|\n"
-    text += SPLIT_LINE
+class Question:
+    def __init__(self, question: str, answer: str, difficulty: int):
+        self.question = question
+        self.answer = answer
+        self.difficulty = difficulty
 
-    return text
+    def print(self):
+        print(self.question)
+
+    def check_answer(self, user_answer: str) -> bool:
+        return self.answer.lower() == user_answer.lower()
+
+
+class Game:
+    def __init__(self, user: User, questions: list[Question]):
+        self.score = 0
+        self.user = user
+        self.start_time = datetime.now()
+        self.questions = questions
+        self.index_of_questions = 0
+        self.last_index = len(questions) - 1
+
+    def print_question(self):
+        self.current_question = self.questions[self.index_of_questions]
+        self.current_question.print()
+
+    def user_answer(self):
+        user_input = input(": ")
+        if self.current_question.check_answer(user_input):
+            self.score += self.current_question.difficulty
+            print("ОК")
+        else:
+            print("НЕ ОК")
+        self.index_of_questions += 1
 
 
 def main():
-    text = format(NAMES)
-    print(text)
+    name = input("Имя: ")
+    user = User(name)
+    questions = [
+        Question("Столица России?", "Москва", 1),
+        Question("Столица Франции?", "Париж", 2),
+    ]
+    game = Game(user, questions)
+    while game.index_of_questions <= game.last_index:
+        game.print_question()
+        game.user_answer()
 
 
-if __name__ == "__main__":
-    main()
+main()
