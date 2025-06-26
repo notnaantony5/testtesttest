7665a7fdb4e9ef27839b51e35f6f185aeca1acb0
TEST <test@test.com>
2025-06-26 19:12:50 +0300

    init

--- main.py
+++ main.py
@@ -1,61 +1,51 @@
-from datetime import datetime
+from dataclasses import dataclass
+
+
+class Service:
+    endpoint = "test"
+
+    @classmethod
+    def call(cls):
+        print(cls.endpoint)
+
+
+# Service.call()
+
+
+@dataclass
+class UserData:
+    login: str
+    age: int
 
 
 class User:
-    """fasdgas"""
+    def __init__(self, username: str, age: int):
+        self.username = username
+        self.age = age
 
-    def __init__(self, name: str):
-        self.name = name
-
-
-class Question:
-    def __init__(self, question: str, answer: str, difficulty: int):
-        self.question = question
-        self.answer = answer
-        self.difficulty = difficulty
-
-    def print(self):
-        print(self.question)
-
-    def check_answer(self, user_answer: str) -> bool:
-        return self.answer.lower() == user_answer.lower()
+    @classmethod
+    def from_userdata(cls, user_data: UserData):
+        return cls(user_data.login, user_data.age)
 
 
-class Game:
-    def __init__(self, user: User, questions: list[Question]):
-        self.score = 0
-        self.user = user
-        self.start_time = datetime.now()
-        self.questions = questions
-        self.index_of_questions = 0
-        self.last_index = len(questions) - 1
-
-    def print_question(self):
-        self.current_question = self.questions[self.index_of_questions]
-        self.current_question.print()
-
-    def user_answer(self):
-        user_input = input(": ")
-        if self.current_question.check_answer(user_input):
-            self.score += self.current_question.difficulty
-            print("ОК")
-        else:
-            print("НЕ ОК")
-        self.index_of_questions += 1
+user_data = UserData("test", 22)
+user = User.from_userdata(user_data)
+print(user.age)
 
 
-def main():
-    name = input("Имя: ")
-    user = User(name)
-    questions = [
-        Question("Столица России?", "Москва", 1),
-        Question("Столица Франции?", "Париж", 2),
-    ]
-    game = Game(user, questions)
-    while game.index_of_questions <= game.last_index:
-        game.print_question()
-        game.user_answer()
-    print((datetime.now() - game.start_time).total_seconds())
+class UserService:
+    atrib = 22
+
+    @staticmethod
+    def func():
+        return
 
 
-main()
+class PostService:
+    @staticmethod
+    def func(text: str):
+        return text
+
+
+PostService.func("text")
+UserService.func()
