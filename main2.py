21aa05059482b99d5bfc08a13f3f6d76d3a910a6
TEST <test@test.com>
2025-06-26 20:37:37 +0300

    init

--- main2.py
+++ main2.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 import json
+from typing import Any
 
 
 @dataclass
@@ -13,18 +14,32 @@ class UserService:
     def __init__(self, path="users.json"):
         self.path = path
 
-    def get_users_data(self) -> list[UserData]:
-        result = []
+    def _get_dicts_user_data(self) -> list[dict[str, Any]]:
         with open(self.path, "r", encoding="utf-8") as f:
-            for user_data in json.load(f):
-                result.append(UserData(**user_data))
+            return json.load(f)
+
+    def get_users_data(self) -> list[UserData]:
+        user_dicts = self._get_dicts_user_data()
+        result = []
+        for user_data in user_dicts:
+            result.append(UserData(**user_data))
         return result
 
-    def get_by_username(self, username: str):
-        user_datas = self.get_users_data()
+    def get_by_username(self, username: str) -> UserData | None:
+        user_dicts = self._get_dicts_user_data()
+        for user_dict in user_dicts:
+            if user_dict.get("username") == username:
+                return UserData(**user_dict)
+
+    def add_user_data(self, user_data: UserData) -> None:
+        user_dicts = self._get_dicts_user_data()
+        user_dicts.append(user_data.__dict__)
+        with open(self.path, "w", encoding="utf-8") as f:
+            json.dump(user_dicts, f, ensure_ascii=False, indent=2)
 
 
 service = UserService()
-print(service.get_by_username("test"))
-# UserData(username='test', fullname='TEST TEST', rating=15)
+print("search", service.get_by_username("sasha2"))
 print(service.get_users_data())
+user_data = UserData("sasha2", "впвыа", 13)
+service.add_user_data(user_data)
