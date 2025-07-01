af9438d2c59baa73997e3536423c946696af265f
TEST <test@test.com>
2025-07-01 21:07:56 +0300

    update

--- main5.py
+++ main5.py
@@ -6,7 +6,7 @@ print(
             (sqrt(x**2 + y**2), (x, y))
             for x, y in (
                 tuple(map(int, line.strip().split(", ")))
-                for line in open("coord.txt", "r", encoding="utf-8").readlines()
+                for line in open("coord.txt", "r", encoding="utf-8")
             )
         ),
         key=lambda x: x[0],
