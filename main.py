2f0eb9289adc9f163920cf8826c36aa135beeb0f
TEST <test@test.com>
2025-07-03 19:26:52 +0300

    инит

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Generator
 
 
 INPUT_PATH = "names.txt"
+OUTPUT_PATH = "result_names.txt"
 
 
 def generate_lines_from_file(path: str = INPUT_PATH) -> Generator[str, None, None]:
@@ -16,11 +17,20 @@ def filter_by_a(generator: Generator[str, None, None]) -> Generator[str, None, N
             yield text
 
 
+def transform(generator: Generator[str, None, None]) -> Generator[str, None, None]:
+    for text in generator:
+        lastname, firstname = text.split(" ")
+        firstname = firstname[0] + "."
+        yield lastname + " " + firstname
+
+
 def main():
     gen = generate_lines_from_file()
     filt = filter_by_a(gen)
-    for line in filt:
-        print(line)
+    result = transform(filt)
+    with open(OUTPUT_PATH, "w", encoding="utf-8") as f:
+        for line in result:
+            f.write(line + "\n")
 
 
 if __name__ == "__main__":
