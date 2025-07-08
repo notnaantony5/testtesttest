5ef7ef102d3eb5d009919c164f0c50dd0e3b90f5
TEST <test@test.com>
2025-07-08 20:41:30 +0300

    init

--- main3.py
+++ main3.py
@@ -3,9 +3,21 @@ from sqlite3 import connect
 con = connect("db.sqlite3")
 
 cu = con.cursor()
-COMMAND = """ 
-CREATE TABLE test(
-    id INT PRIMARY KEY
+CREATE_TABLE_COMMAND = """ 
+CREATE TABLE student (
+    id INTEGER PRIMARY KEY,
+    username TEXT,
+    fullname TEXT,
+    CONSTRAINT username_constraint UNIQUE (username)
 )
 """
-cu.execute(COMMAND)
+DELETE_COMMAND = """
+DROP TABLE student
+"""
+CREATE_COMMAND = """
+INSERT INTO student (username, fullname)
+VALUES (?, ?)
+"""
+# cu.execute(CREATE_TABLE_COMMAND)
+cu.execute(CREATE_COMMAND, ("sasha2", "Александр"))
+con.commit()
