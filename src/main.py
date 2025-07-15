9b5c0b7c55c035092d27a948ee12e7f07f45bca0
TEST <test@test.com>
2025-07-15 18:44:29 +0300

    init

---
+++ src/main.py
@@ -0,0 +1,29 @@
+MENU = """1. ---
+0. Выход"""
+MENU_CHOICES = {0, 1}
+
+
+def get_user_choice() -> int:
+    while True:
+        try:
+            user_input = int(input().strip())
+            if user_input not in MENU_CHOICES:
+                raise ValueError
+        except ValueError:
+            print("Это не пункт меню! Попробуйте еще раз.")
+        else:
+            return user_input
+
+
+def main():
+    print("Привет! Это программа для помощи учета товара на складе")
+    while True:
+        print(MENU)
+        user_input = get_user_choice()
+        if user_input == 0:
+            break
+    print("Завершение работы программы...")
+
+
+if __name__ == "__main__":
+    main()
